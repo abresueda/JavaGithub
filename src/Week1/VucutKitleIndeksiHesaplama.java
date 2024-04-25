d5eafff183cfdd7f5b3c40fc55325783ceaa030e
Abre Süeda Özmen <abre.sueda@gmail.com>
2024-04-25 16:37:06 +0300

    Aritmetik işlemler yapıldı.

--- src/Week1/VucutKitleIndeksiHesaplama.java
+++ src/Week1/VucutKitleIndeksiHesaplama.java
@@ -1,18 +1,22 @@
 package Week1;
 
 import java.util.Scanner;
+// Vücut Kitle İndeksini hesaplamak için bir program yaz.
 
-public class VücutKitleIndeksiHesaplama {
+public class VucutKitleIndeksiHesaplama {
     public static void main(String[] args) {
         Scanner inp=new Scanner(System.in);
-
+        //Değişkenleri giriniz.
         double kg,m;
+
+        //Kullanıcıdan girdileri alınız.
         System.out.print("Lütfen boyunuzu metre cinsinden giriniz: ");
         m=inp.nextDouble();
 
         System.out.print("Lütfen kilonuzu giriniz: ");
         kg=inp.nextDouble();
 
+        //İndeksi tanımlayınız.
         double indeks;
         indeks=(kg/(m*m));
         System.out.print("Vücut Kitle İndeksiniz: " + indeks);
